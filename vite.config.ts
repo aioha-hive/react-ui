e1f13245affe057a165dc0c7493bdd30c40a2e3e
techcoderx <engtianxi@outlook.com>
2024-06-19 21:29:51 +0800

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
+import { peerDependencies } from './package.json'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: Object.keys(peerDependencies)
+    }
+  },
+  plugins: [react(), dts({ include: ['lib'] })]
+})
