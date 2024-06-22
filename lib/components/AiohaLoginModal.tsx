2ef1f025ae9f65ef45e5373e313a0f5f7b6a6a54
techcoderx <engtianxi@outlook.com>
2024-06-22 14:21:41 +0800

    center modal on screen, close modal

--- lib/components/AiohaLoginModal.tsx
+++ lib/components/AiohaLoginModal.tsx
@@ -1,9 +1,12 @@
+import { Dispatch, SetStateAction } from 'react'
+
 interface LoginModalProps {
   displayed?: boolean
   title?: string
+  onClose: Dispatch<SetStateAction<boolean>>
 }
 
-export const AiohaLoginModal = ({ displayed = false, title = 'Connect Wallet' }: LoginModalProps) => {
+export const AiohaLoginModal = ({ displayed = false, title = 'Connect Wallet', onClose }: LoginModalProps) => {
   return (
     <div
       id="aioha-modal"
@@ -11,16 +14,18 @@ export const AiohaLoginModal = ({ displayed = false, title = 'Connect Wallet' }:
       aria-hidden="true"
       className={`${
         displayed ? '' : 'hidden'
-      } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-20`}
+      } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-20`}
+      onClick={() => onClose(false)}
     >
       <div className="relative p-4 w-full max-w-md max-h-full">
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700" onClick={(e) => e.stopPropagation()}>
           <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{title}</h3>
             <button
               type="button"
               className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
               data-modal-toggle="aioha-modal"
+              onClick={() => onClose(false)}
             >
               <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import { useState } from 'react'
 import { AiohaLoginModal } from '../lib'
 
 export const App = () => {
+  const [aiohaModalDisplayed, setAiohaModalDisplayed] = useState(false)
   return (
     <div className="dark">
       <div className="min-h-screen min-w-full dark:bg-gray-800">
-        <AiohaLoginModal displayed />
+        <button
+          type="button"
+          data-modal-target="crypto-modal"
+          data-modal-toggle="crypto-modal"
+          className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
+          onClick={() => setAiohaModalDisplayed(true)}
+        >
+          <svg
+            aria-hidden="true"
+            className="w-4 h-4 me-2"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+            ></path>
+          </svg>
+          Connect wallet
+        </button>
+        <AiohaLoginModal displayed={aiohaModalDisplayed} onClose={setAiohaModalDisplayed} />
       </div>
     </div>
   )
