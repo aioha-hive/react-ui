ef399576a7a29dfdefd69d4f52c44ca7f77d6c1b
techcoderx <engtianxi@outlook.com>
2024-06-26 16:02:31 +0800

    hivesigner cb page component

---
+++ lib/components/HiveSignerCb.tsx
@@ -0,0 +1,6 @@
+import { hivesignerCb } from '@aioha/aioha/build/lib/hivesigner-cb'
+
+export const HiveSignerCb = () => {
+  hivesignerCb()
+  return <p className="ml-2 text-black dark:text-white">Redirecting back to Aioha app...</p>
+}
