eb87d4857493c9dbbd03f09f31b07b7e8e788085
techcoderx <engtianxi@outlook.com>
2024-06-30 09:39:03 +0800

    polyfill buffer

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import tailwindcss from 'tailwindcss'
 import { peerDependencies } from './package.json'
 
@@ -16,7 +17,15 @@ export default defineConfig({
       external: Object.keys(peerDependencies)
     }
   },
-  plugins: [react(), dts({ include: ['lib'] })],
+  plugins: [
+    react(),
+    dts({ include: ['lib'] }),
+    nodePolyfills({
+      globals: {
+        Buffer: true
+      }
+    })
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()]
