b42a1b3d398ead77d5a2a919abe6b43796e35942
techcoderx <git@techcoderx.com>
2025-05-02 10:43:28 +0800

    example sign prompt modal

---
+++ src/prompts/PromptIcons.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export const PenIcon = ({ size = 6 }: { size?: number }) => {
+  return (
+    <svg
+      className={`size-${size} text-gray-800 dark:text-white mx-auto`}
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1"
+        d="m4.988 19.012 5.41-5.41m2.366-6.424 4.058 4.058-2.03 5.41L5.3 20 4 18.701l3.355-9.494 5.41-2.029Zm4.626 4.625L12.197 6.61 14.807 4 20 9.194l-2.61 2.61Z"
+      />
+    </svg>
+  )
+}
+
+export const CircleArrowUp = ({ size = 6 }: { size?: number }) => (
+  <svg
+    className={`size-${size} text-gray-800 dark:text-white mx-auto`}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M16 12L12 8M12 8L8 12M12 8V16M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
+      stroke="currentColor"
+      strokeWidth="1.3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default CircleArrowUp
---
+++ src/prompts/SignModal.tsx
@@ -0,0 +1,55 @@
+import React, { Dispatch, SetStateAction } from 'react'
+import { CloseIcon } from '../../lib/icons/CloseIcon'
+import { CircleArrowUp, PenIcon } from './PromptIcons'
+
+export const SignModal = ({
+  displayed = false,
+  trx_id,
+  explorerUrl = 'https://vsc.techcoderx.com',
+  onClose
+}: {
+  displayed?: boolean
+  trx_id?: string
+  explorerUrl?: string
+  onClose: Dispatch<SetStateAction<boolean>>
+}) => {
+  return (
+    <div
+      id="aioha-sign-modal"
+      tabIndex={-1}
+      className={`${
+        displayed ? '' : 'hidden'
+      } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-full bg-black bg-opacity-30`}
+    >
+      <div className={`relative p-4 max-w-md max-h-full`}>
+        <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700 min-w-sm">
+          <button
+            type="button"
+            className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            onClick={() => onClose(false)}
+            aria-label="Close"
+          >
+            <CloseIcon />
+          </button>
+          <div className="p-4 md:p-8 flex flex-col gap-5 place-content-center text-center">
+            {trx_id ? <CircleArrowUp size={28} /> : <PenIcon size={28} />}
+            <div className="flex flex-col gap-1">
+              <div className="text-xl text-gray-900 dark:text-gray-100 text-center">
+                {trx_id ? 'Transaction Sent' : 'Please sign transaction in your wallet'}
+              </div>
+              {trx_id && (
+                <a
+                  href={`${explorerUrl}/tx/${trx_id}`}
+                  target={'_blank'}
+                  className="font-sm text-gray-900 dark:text-gray-100 hover:underline text-center"
+                >
+                  View in explorer
+                </a>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
